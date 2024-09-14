9f904d04afadb49651dc882e257a5cf2a072c5af
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2024-09-14 15:57:25 +0530

    Remove doc_category1.js and replace script link by about.js in doc_category1.html

--- doc_category1.js
+++
@@ -1,8 +0,0 @@
-/* ------------------ NAVIGATION BAR ------------------ */
-
-const btn= document.getElementById("btn")
-const list= document.querySelector(".list")
-
-btn.addEventListener("click", ()=>{
-  list.classList.toggle("show")
-});
